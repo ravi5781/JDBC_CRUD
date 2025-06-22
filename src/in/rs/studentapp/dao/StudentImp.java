70a5e449d87a10e487e5ba1b2dfadc2b9fc83498
ravi5781 <ravikiransh018@gmail.com>
2025-06-23 00:15:40 +0530

    fifth commit

--- src/in/rs/studentapp/dao/StudentImp.java
+++ src/in/rs/studentapp/dao/StudentImp.java
@@ -41,53 +41,88 @@ public class StudentImp implements StudentDAO {
 
 	@Override
 	public boolean updateStudent(Student s) {
-		// TODO Auto-generated method stub
-		return false;
+		PreparedStatement ps = null;
+		String query = "UPDATE STUDENT SET NAME=?,PHONE=?,MAIL=?,BRANCH=?,LOC=?,PASSWORD=?,DATE=SYSDATE() WHERE ID=?";
+		int res = 0;
+		try {
+			ps = con.prepareStatement(query);
+			ps.setString(1, s.getName());
+			ps.setLong(2, s.getPhone());
+			ps.setString(3, s.getMail());
+			ps.setString(4, s.getBranch());
+			ps.setString(5, s.getLoc());
+			ps.setString(6, s.getPassword());
+			ps.setInt(7, s.getId());
+			res = ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (res > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 	@Override
 	public boolean deleteStudent(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		PreparedStatement ps = null;
+		String query = "DELETE FROM STUDENT WHERE ID=? AND ID!=1";
+		int res = 0;
+		try {
+			ps = con.prepareStatement(query);
+			ps.setInt(1, id);
+			res = ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (res > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public Student getStudent(String mail, String password) {
 		// TODO Auto-generated method stub
-		PreparedStatement ps=null;
-		 String query="SELECT * FROM STUDENT WHERE MAIL=? AND PASSWORD=?";
-		 Student s=null;
-		 try {
-		 ps=con.prepareStatement(query);
-		 ps.setString(1,mail);
-		 ps.setString(2, password);
-		 ResultSet rs=ps.executeQuery();
-		 while(rs.next()) {
-		 s=new Student();
-		//int id=rs.getInt("id");
-		 //s.setId(id);
-		 s.setId(rs.getInt("id"));
-		 s.setName(rs.getString("name"));
-		 s.setPhone(rs.getLong("phone"));
-		 s.setMail(rs.getString("mail"));
-		 s.setBranch(rs.getString("branch"));
-		 s.setLoc(rs.getString("loc"));
-		 s.setPassword(rs.getString("password"));
-		 s.setDate(rs.getString("date"));
-		 }
+		PreparedStatement ps = null;
+		String query = "SELECT * FROM STUDENT WHERE MAIL=? AND PASSWORD=?";
+		Student s = null;
+		try {
+			ps = con.prepareStatement(query);
+			ps.setString(1, mail);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				s = new Student();
+				// int id=rs.getInt("id");
+				// s.setId(id);
+				s.setId(rs.getInt("id"));
+				s.setName(rs.getString("name"));
+				s.setPhone(rs.getLong("phone"));
+				s.setMail(rs.getString("mail"));
+				s.setBranch(rs.getString("branch"));
+				s.setLoc(rs.getString("loc"));
+				s.setPassword(rs.getString("password"));
+				s.setDate(rs.getString("date"));
+			}
 
-		 } catch (SQLException e) {
-		 // TODO Auto-generated catch block
-		 e.printStackTrace();
-		 }
-		 return s;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return s;
 
 	}
 
 	@Override
 	public Student getStudent(long phone, String mail) {
 		// TODO Auto-generated method stub
-		
+
 		return null;
 	}
 
